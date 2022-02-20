b78b57c4c6b39ee290ab3d7aa3807b54a40d6461
jhunderdog <33382water@gmail.com>
2022-02-20 20:41:50 +0900

    Testing createAccount part Two

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,13 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+
+jest.mock("got", () => {
+  return {
+    post: jest.fn(),
+  }
+})
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
@@ -48,6 +55,30 @@ describe('UserModule (e2e)', () => {
         expect(res.body.data.createAccount.error).toBe(null)
       })
     });
+
+    it('should fail if the account exists', ()=> {
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+        mutation {
+          createAccount(input: {
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false);
+        expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+      })
+    })
   })
   it.todo('userProfile')
   it.todo('login')
