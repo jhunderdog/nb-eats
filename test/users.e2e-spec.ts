8bb01f15972c36cfeefbc71ac58c3d1dfc28120e
jhunderdog <33382water@gmail.com>
2022-02-21 10:02:07 +0900

    Testing userProfile

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,8 +1,10 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 
 jest.mock("got", () => {
@@ -20,6 +22,7 @@ const testUser = {
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let usersRespotiry: Repository<User>
   let jwtToken: string;
 
   beforeAll(async () => {
@@ -28,6 +31,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    usersRespotiry = module.get<Repository<User>>(getRepositoryToken(User))
     await app.init();
   });
 
@@ -147,7 +151,66 @@ describe('UserModule (e2e)', () => {
       })
     });
   });
-  it.todo('userProfile');
+  describe('userProfile', () => {
+    let userId: number;
+    beforeAll(async () => {
+      const [user] = await usersRespotiry.find();
+      userId = user.id;
+    });
+    it("should see a user's profile", () => {
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .set(`X-JWT`, jwtToken)
+      .send({
+        query: `
+        {
+          userProfile(userId:${userId}){
+            ok
+            error
+            user {
+              id
+            }
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        const {body: {data: {userProfile: {ok, error, user: {id}}}}} = res;
+        console.log(res);
+        expect(ok).toBe(true);
+        expect(error).toBe(null);
+        expect(id).toBe(userId);
+      })
+    })
+    it("should not find a profile", () => {
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .set(`X-JWT`, jwtToken)
+      .send({
+        query: `
+        {
+          userProfile(userId:666){
+            ok
+            error
+            user {
+              id
+            }
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        const {body: {data: {userProfile: {ok, error, user}}}} = res;
+        console.log(res);
+        expect(ok).toBe(false);
+        expect(error).toBe("User Not Found");
+        expect(user).toBe(null);
+        
+      })
+    });
+  });
   
   it.todo('me')
   it.todo('verifyEmail')
