de1131b18f982af636da78a6f9b8980316cea840
jhunderdog <33382water@gmail.com>
2022-03-02 21:40:37 +0900

    Subscription Authentication part Two

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,11 +4,17 @@ import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { Observable } from "rxjs";
 import { User } from 'src/users/entities/user.entity';
+import { JwtService } from 'src/jwt/jwt.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector){}
-    canActivate(context: ExecutionContext){
+    constructor(
+        private readonly reflector: Reflector,
+        private readonly jwtService:JwtService,
+        private readonly usersService:UsersService
+    ){}
+    async canActivate(context: ExecutionContext){
         const roles = 
         this.reflector.get<AllowedRoles>(
             'roles', 
@@ -19,15 +25,25 @@ export class AuthGuard implements CanActivate {
             return true;
         }
         const gqlContext = GqlExecutionContext.create(context).getContext();
-        console.log(gqlContext.token);
-        const user: User = gqlContext['user'];
-        // console.log(user);
-        if(!user){
-            return false;
-        }  
-        if(roles.includes("Any")){
-            return true;
-        }      
-        return roles.includes(user.role);
+        const token = gqlContext.token;
+        if(token) {
+            const decoded = this.jwtService.verify(token.toString());
+            if(typeof decoded === "object" && decoded.hasOwnProperty('id')){                
+            const { user } = await this.usersService.findById(decoded["id"]);
+            if(!user){
+                return false;
+            }
+            gqlContext['user'] = user;  
+            if(roles.includes("Any")){
+                return true;
+            }      
+            return roles.includes(user.role);    
+            } else {
+                false;
+            }
+        } else {
+            return false
+        }
+                
     }
 }
